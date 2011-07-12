d2d95d6ee78d31d147f772d16846a6b7f62b82fc
Francois de la Taste <francois@opensailing.net>
2011-07-12 20:17:49 +0200

    licensed program under GPLv3
    added COPYING with full license + header in protei.c

--- protei.c
+++ protei.c
@@ -1,3 +1,20 @@
+/*
+	Protei — Remote Control and Motor Control
+    Copyright (C) 2011  Logan Williams, Gabriella Levine, Qiuyang Zhou
+
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 // motors are indexed as follow:
 //  0 - bow articulation motor
 //  1 - stern articulation motor
